aad4707bb06b084459180c67ae7a36e8985e8ec8
Thanasis Karezos <thankarezos@gmail.com>
2023-04-23 03:12:45 +0300

    fix

--- machinelearning101/train.py
+++ machinelearning101/train.py
@@ -8,7 +8,6 @@ import plots as pl
 def training_finished(model, X_train, y_train, X_test, y_test):
     
     plt.close()
-    print(model.trained)
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
     fig.suptitle(model.name, fontsize=25)
     fig.text(0.5, 0.92, f"Learning Rate: {model.learning_rate}", ha='center', fontsize=14)
@@ -18,7 +17,7 @@ def training_finished(model, X_train, y_train, X_test, y_test):
     pl.fit_plot4_static(model, X_train, y_train, X_test, y_test, axs[1,1])
     plt.show()
 
-def per_epoch(model, X_train, y_train, X_test, y_test):
+def per_epoch(model, X_train, y_train, X_test, y_test, callback=None):
     
     fig = plt.figure(figsize=(12, 8))
     fig.suptitle(model.name, fontsize=25)
@@ -27,20 +26,18 @@ def per_epoch(model, X_train, y_train, X_test, y_test):
     axs = [fig.add_subplot(gs[0, 0]), fig.add_subplot(gs[0, 1]), fig.add_subplot(gs[1, :])]
     pl.fit_plot1_static(model, X_test, y_test, axs[0])
     anim2 = pl.fit_plot2(model, X_train, y_train, X_test, axs[1], fig, active=True)
-    callback = lambda: training_finished(model,  X_train, y_train, X_test, y_test)
     anim3 = pl.fit_plot3(model, X_train, y_train, X_test, y_test, axs[2], fig, callback=callback)
     plt.show()
-    print("test")
 
 def linearSeparated(n, model):
     X = cd.linearSeparated(n)
     X_train, y_train, X_test, y_test = cd.splitData(X)
-    per_epoch(model, X_train, y_train, X_test, y_test)
+    callback = lambda: training_finished(model,  X_train, y_train, X_test, y_test)
+    per_epoch(model, X_train, y_train, X_test, y_test, callback=callback)
 
 
-perceptron = Perceptron(learning_rate=0.01, num_epochs=100)
-linearSeparated(504, perceptron)
-print(perceptron.trained)
+# perceptron = Perceptron(learning_rate=0.01, num_epochs=100)
+# linearSeparated(504, perceptron)
 
-# adaline = Adaline(learning_rate=0.001, num_epochs=100)
-# linearSeparated(504,adaline)
+adaline = Adaline(learning_rate=0.001, num_epochs=100)
+linearSeparated(504,adaline)
