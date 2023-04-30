c410708827e0c63bdbc239ae819330e744a59f5b
Thanasis Karezos <thankarezos@gmail.com>
2023-04-30 07:05:19 +0300

    finished

--- A2.py
+++ A2.py
@@ -17,34 +17,53 @@ def df(x):
 
 # Define Gradient Descent
 def gradient_descent(f, df, x_init, learning_rate, num_iterations):
-    x = x_init
+    x = np.array(x_init, dtype=np.float64)
+    x_history = [x] # to store the history of x values
     for i in range(num_iterations):
         x = x - learning_rate * df(x)
-    return x
+        x_history.append(x)
+    return x, np.array(x_history)
 
-x_init = np.array([-2, 2])
+def plot():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    x = np.linspace(-4, 4, 100)
+    y = np.linspace(-4, 4, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = f([X, Y])
+    ax.plot_surface(X, Y, Z, cmap='coolwarm', zorder=-1)
+    
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    scatter = None
+    plot = None
+
+    # Define the update function
+    def update(frame):
+        nonlocal scatter
+        nonlocal plot
+    
+        x_min, x_history = gradient_descent(f, df, x_init, learning_rate, frame+1)
+
+        if plot:
+            plot[0].remove()
+        plot = ax.plot([x_min[0]], [x_min[1]], [f(x_min)], marker='o', markersize=10, color='black', zorder=5)
+        return scatter, plot 
+
+    # Create the animation
+    anim = FuncAnimation(fig, update, frames=num_iterations, interval=200)
+
+    # Show the animation
+    plt.show()
+
+
+x_init = np.array([0.5, -0.5])
 
 learning_rate = 0.1
 num_iterations = 100
 
-x_min = gradient_descent(f, df, x_init, learning_rate, num_iterations)
+plot()
 
-# Print the minimum point
-print("Minimum point: ", x_min)
-
-# Define the x and y values for plotting
-x = np.linspace(-4, 4, 100)
-y = np.linspace(-4, 4, 100)
-X, Y = np.meshgrid(x, y)
-Z = f([X, Y])
-
-# Plot the function and the minimum point
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, Z, cmap='coolwarm')
-ax.scatter(x_min[0], x_min[1], f(x_min), color='black', s=200,)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
-plt.show()
--- A3.py
+++ A3.py
@@ -1,3 +1,4 @@
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,8 +13,8 @@ def f(x):
 def df(x):
     x1, x2 = x
     return np.array([
-        -x1/np.sqrt(x1**2 + x2**2 + s**2),
-        -x2/np.sqrt(x1**2 + x2**2 + s**2)
+        x1/np.sqrt(x1**2 + x2**2 + s**2),
+        x2/np.sqrt(x1**2 + x2**2 + s**2)
     ])
 
 # Define Gradient Descent
@@ -25,33 +26,47 @@ def gradient_descent(f, df, x_init, learning_rate, num_iterations):
         x_history.append(x)
     return x, np.array(x_history)
 
-# Define initial values
-x_init = np.array([0,0])
+def plot():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-learning_rate = 0.01
+    x = np.linspace(-4, 4, 100)
+    y = np.linspace(-4, 4, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = f([X, Y])
+    ax.plot_surface(X, Y, Z, cmap='coolwarm', zorder=-1)
+    
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    scatter = None
+    plot = None
+
+    # Define the update function
+    def update(frame):
+        nonlocal scatter
+        nonlocal plot
+    
+        x_min, x_history = gradient_descent(f, df, x_init, learning_rate, frame+1)
+
+        if plot:
+            plot[0].remove()
+        plot = ax.plot([x_min[0]], [x_min[1]], [f(x_min)], marker='o', markersize=10, color='black', zorder=5)
+        return scatter, plot 
+
+    # Create the animation
+    anim = FuncAnimation(fig, update, frames=num_iterations, interval=200)
+
+    # Show the animation
+    plt.show()
+
+
+x_init = np.array([4, -4])
+
+learning_rate = 0.1
 num_iterations = 100
 
-x_min, x_history = gradient_descent(f, df, x_init, learning_rate, num_iterations)
-print("Minimum point: ", x_min)
+plot()
 
-x = np.linspace(-4, 4, 100)
-y = np.linspace(-4, 4, 100)
-X, Y = np.meshgrid(x, y)
-Z = f([X, Y])
-
-# Plot the function and the minimum point
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, Z, cmap='coolwarm')
-ax.scatter(x_min[0], x_min[1], f(x_min), color='black', s=200,)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
-# x_history = np.array(x_history)
-# ax.plot(x_history[:,0], x_history[:,1], f(x_history.T), '-o', color='black')
-
-plt.show()
-
-# Define the x values for plotting
 
