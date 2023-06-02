5cd5b68489f5609adbb4bb21c16006694b6151a1
thankarezos <thankarezos@gmail.com>
2023-06-02 23:41:04 +0300

    final

--- hopefield/main.py
+++ hopefield/main.py
@@ -3,42 +3,46 @@ import myHopefield as hf
 import patterns as pt
 
 # hf.print_pattern(pt.patterns[5])
-print("Saved patterns:")
-hf.print_pattern2(pt.patterns)
+def main():
+    print("Saved patterns:")
+    hf.print_pattern2(pt.patterns)
 
 
-print("Chose pattern to recall")
-hf.print_pattern2(pt.patternsMod)
+    print("Chose pattern to recall")
+    hf.print_pattern2(pt.patternsMod)
 
-while True:
-    option = int(input("Give Number: "))
-    if pt.patternsMod[option] is None:
-        print("Pattern not found")
-    else:
-        break
-pattern = pt.patternsMod[option]
+    while True:
+        option = int(input("Give Number: "))
+        if pt.patternsMod[option] is None:
+            print("Pattern not found")
+        else:
+            break
+    pattern = pt.patternsMod[option]
 
-print()
-print(
-"1. Sync\n" +
-"2. Async"
-)
+    print()
+    print(
+    "1. Sync\n" +
+    "2. Async"
+    )
 
 
-async def async_prediction():
-    await asyncio.create_task(hf.async_prediction(pattern))
-while True:
-    option = int(input("Option: "))
-    if option == 1:
-        # hf.print_pattern(pattern)
-        hf.sync_prediction(pattern)
-        break
-    elif option == 2:
-        # hf.print_pattern(pattern)
-        asyncio.run(async_prediction())
-        break
-    else:
-        print("Wrong option")
+    async def async_prediction():
+        await asyncio.create_task(hf.async_prediction(pattern))
+    while True:
+        option = int(input("Option: "))
+        if option == 1:
+            # hf.print_pattern(pattern)
+            hf.sync_prediction(pattern)
+            break
+        elif option == 2:
+            # hf.print_pattern(pattern)
+            asyncio.run(async_prediction())
+            break
+        else:
+            print("Wrong option")
+
+if __name__ == "__main__":
+    main()
 
 
 
